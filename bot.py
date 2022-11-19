c09b3a9d632f3b17b51a6688614300c956979baf
Ahsan Mashruf <killcode57@gmail.com>
2022-11-19 16:12:54 -0600

    migrate from discord.py to pycord

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 import discord
 import os
 from discord.ext import commands
+import logging
 
-client = commands.Bot(command_prefix=".")
+logging.basicConfig(level=logging.DEBUG)
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix=".", intents=intents)
 
 @client.event
 async def on_ready():
@@ -41,4 +45,7 @@ for filename in os.listdir('./cogs'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
 TOKEN = os.getenv('TOKEN')
+
+# set testing bot token here
+# TOKEN = 'TOKEN'
 client.run(TOKEN)
