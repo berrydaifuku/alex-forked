3ba40d3558f1af647b25ef997abf0135dfa04576
Ahsan Mashruf <killcode57@gmail.com>
2022-11-21 15:13:54 -0600

    Added status timestamp

--- cogs/info.py
+++ cogs/info.py
@@ -1,10 +1,12 @@
 import discord
 from discord.ext import commands
+import datetime
 
 class Info(commands.Cog):
 
     def __init__(self, client):
         self.client = client
+        self.startTime = datetime.datetime.now(datetime.timezone.utc)
 
     @commands.slash_command(description="Pong!")
     async def ping(self, ctx):
@@ -18,11 +20,13 @@ class Info(commands.Cog):
             members += guild.member_count - 1
         embed = discord.Embed(
             title="Status",
-            color=discord.Color.blue()
+            color=discord.Color.blue(),
+            #footer= f"Bot started <t:{self.startTime}:R>"
         )
         embed.add_field(name="Servers", value=servers, inline=True)
         embed.add_field(name="Contestants", value=members, inline=True)
-
+        embed.timestamp = self.startTime
+        embed.set_footer(text="Bot started")
         await ctx.respond(embed=embed)
 
 def setup(client):
