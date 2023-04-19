7cfa00b0988a35bec8febc79ec190d37d67913c5
berrydaifuku <114842047+berrydaifuku@users.noreply.github.com>
2023-04-19 11:50:02 +0900

    changes
    added features:
    - checks for question format
    - allows everyone to answer
    - leaderboard + clear leaderboard

--- README.md
+++ README.md
@@ -1,15 +1,8 @@
 # ALEX!
 ## Play jeopardy with your friends!
-### [Click here to add to server](https://discord.com/oauth2/authorize?client_id=526970299108032514&scope=bot)
-
-This bot has exactly one purpose.
 
 **Just type "/q" (sans quotes) for a question.** Answer within a time limit. That simple!
 
 We use pretty permissive fuzzy string matching to validate answers—this means you'll likely get credit for questions you got incorrect every now and then.
 
-This bot is developed and maintained for free by a student (me!). It is entirely open-source, and I will never develop a "pro" or "premium" version that costs money. I pay for hosting out of pocket. If you're enjoying the bot, please consider donating to support the bot's continued development and hosting costs. 
-
-If you're enjoying the bot, please [vote, or leave a review.](https://top.gg/bot/526970299108032514)
-
-[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/T6T6DW473)
+The original bot was written by AeonEcho, support them here: [![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/T6T6DW473)
--- cogs/questions.py
+++ cogs/questions.py
@@ -2,12 +2,18 @@ import discord
 import requests
 import asyncio
 import collections
+import re
+import time
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 from discord.ext import commands
 
 class QandA(commands.Cog):
     scores = collections.defaultdict(int)
+
+    QUESTION_WORD_REGEX = "/^(what is|what are|whats|what's|where is|where are|wheres|where's|who is|who are|whos|who's|when is|when are|whens|when's|why is|why are|whys|why's) /i"
+    SIMILARITY_THRESHOLD = 0.85
+    QUESTION_ANSWER_TIME = 45
 
     def __init__(self, client):
         self.client = client
@@ -19,7 +25,29 @@ class QandA(commands.Cog):
         s = s.replace('</b>', '**')
         return s
 
-    @commands.slash_command(brief="Get a question." ,description="Get a question. Answer within 30 seconds.")
+    def isQuestionFormat(self, s):
+        return re.search(self.QUESTION_WORD_REGEX, re.sub("/[^\w\s]/i", "", s))
+
+    def isAnswerCorrect(self, answer, correct_answer):
+        # strip question words and punctuation 
+        answer = re.sub("/[^a-zA-Z0-9 ]/g", "", re.sub(self.QUESTION_WORD_REGEX, "", answer))
+
+        # if there are alternative answers, test for substring
+        parentheses_regex = "/\(([^)]+)\)/"
+        if (re.search(parentheses_regex, answer)):
+            diff_alt = fuzz.partial_ratio(answer, correct_answer)
+            if (diff_alt > 0.95):
+                return True
+
+        # otherwise calculate diff 
+        diff = fuzz.ratio(answer, correct_answer)
+        print(f'{correct_answer} = {answer}\n {diff}% match')
+        if (diff > self.SIMILARITY_THRESHOLD):
+            return True
+
+        return False
+
+    @commands.slash_command(brief="get a question." ,description="get a question, answer within 30 seconds.")
     async def q(self, ctx):
         URL = "http://jservice.io/api/random"
         r = requests.get(url=URL)
@@ -40,39 +68,66 @@ class QandA(commands.Cog):
         await ctx.respond(embed=embed)
 
         def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel 
+            m.channel == ctx.channel 
             
+        start = time.time()
+        while (time.time() - start < self.QUESTION_ANSWER_TIME):
+            remaining_time = self.QUESTION_ANSWER_TIME- (time.time - start())
+            if (remaining_time > 1):
+                try:
+                    msg = await self.client.wait_for('message', check=check, timeout=remaining_time)
+                except asyncio.TimeoutError:
+                    timeout = discord.Embed(title="Time's up!", description=f"We were looking for \"{answer}\"", color=0xff0000)
+                    await  ctx.respond(embed=timeout)
+                else:
+                    if (msg.content.lower() == "skip"):
+                        skipped = discord.Embed(title="Skipped", description=f"The ansewr was \"{answer}\"", color=0xff0000)
+                        await ctx.respond(embed=skipped)
+                        break
+                    if self.isQuestionFormat(msg.content):
+                        not_question = discord.Embed(title="Not a question!", description="The answer must be formatted as a question", color=0xff0000)
+                        await ctx.respond(embed=not_question)
+                    elif self.isAnswerCorrect(msg.content, answer):
+                        correct = discord.Embed(title="Correct!", description=f"You got it! The answer was \"{answer}\"", color=0x00ff00)
+                        await ctx.respond(embed=correct)
+                        #await ctx.send('Correct!')
+                        self.scores[msg.author] += value
+                        break
+                    else:
+                        incorrect = discord.Embed(title="Incorrect!", description=f"Any other guesses?", color=0xff0000)
+                        await ctx.respond(embed=incorrect)
+                        #await ctx.send(f"Incorrect.\nThe answer was {answer}")
 
-        try:
-            msg = await self.client.wait_for('message', check=check, timeout=30.0)
-        except asyncio.TimeoutError:
-            timeout = discord.Embed(title="Time's up!", description=f"We were looking for \"{answer}\"", color=0xff0000)
-            await  ctx.respond(embed=timeout)
-            #await ctx.send(f"Sorry! Time's up!\nThe answer was \"{answer}\"")
-        else:
-            diff = fuzz.WRatio(msg.content, answer)
-            print(f'{msg.content} = {answer}\n {diff}% match')
-            if diff > 70:
-                correct = discord.Embed(title="Correct!", description=f"You got it! The answer was \"{answer}\"", color=0x00ff00)
-                await ctx.respond(embed=correct)
-                #await ctx.send('Correct!')
-                self.scores[msg.author] += value
-            else:
-                incorrect = discord.Embed(title="Incorrect!", description=f"We were looking for \"{answer}\"", color=0xff0000)
-                await ctx.respond(embed=incorrect)
-                #await ctx.send(f"Incorrect.\nThe answer was {answer}")
-
-    @commands.slash_command(description="See your score.")
+    @commands.slash_command(description="see your score.")
     async def score(self, ctx):
-        print(self.scores)
-        #create scoreboard embed
-        embed=discord.Embed(title="Scoreboard", color=0x004cff)
+        embed=discord.Embed(title="your score", color=0x004cff)
         if ctx.author in self.scores:
             embed.add_field(name=ctx.author.name, value=self.scores[ctx.author], inline=False)
         else:
             embed.add_field(name=ctx.author.name, value=0, inline=False)
 
         await ctx.respond(embed=embed)
+
+    @commands.slash_command(description="see leaderboard.")
+    async def leaderboard(self, ctx):
+        embed=discord.Embed(title="leaderboard", color=0x004cff)
+
+        num_entries = min(5, len(self.scores))
+        i = 0
+        for author in sorted(self.scores, key=self.scores.get, reverse=True):
+            if (i < num_entries):
+                embed.add_field(name=author, value=self.scores[author], inline=False)
+            else:
+                break
+        
+        await ctx.respond(embed=embed)
+
+    @commands.slash_command(description="clear leaderboard.") 
+    async def clearboard(self, ctx):
+        self.scores = collections.defaultdict(int)
+        embed = discord.Embed(title="leaderboard cleared", color=0x004cff)
+        await ctx.respond(embed=embed)
+
 
 def setup(client):
     client.add_cog(QandA(client))
